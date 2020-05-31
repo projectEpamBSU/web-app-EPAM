4d7c667f00da668d80b643260bd9e1da4c1d9e1a
KuzyaCat <lizerd34@mail.ru>
2020-05-31 22:55:11 +0300

    feat: add current patient profile display

--- Hospital/src/main/java/controller/web/PatientAppointmentsController.java
+++ Hospital/src/main/java/controller/web/PatientAppointmentsController.java
@@ -1,18 +1,17 @@
 package main.java.controller.web;
 
 import main.java.user.Patient;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpSession;
 
-@Controller
-public class PatientProfileController {
-    @GetMapping(value = {"/patient/patient_profile"})
+public class PatientAppointmentsController {
+    @GetMapping(value = {"/patient/patient_appointments"})
     public String getPatientMenu(Model model, HttpSession session) {
         Patient patient = (Patient) session.getAttribute("user");
-        model.addAttribute("patientProfile", patient.showProfile());
-        return "patient_profile";
+        model.addAttribute("patientAppointments", patient.getAppointments());
+
+        return "patient_appointments";
     }
 }
--- Hospital/src/main/java/controller/web/PatientController.java
+++ Hospital/src/main/java/controller/web/PatientController.java
@@ -1,12 +1,14 @@
 package main.java.controller.web;
 
+import main.java.user.Patient;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PatientController {
     @GetMapping(value = {"/patient"})
-    public String getPatientMenu() {
+    public String getPatientMenu(Model model) {
         return "/patient";
     }
 }
